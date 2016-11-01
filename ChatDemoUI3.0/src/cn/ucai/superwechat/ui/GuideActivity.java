bc360136c25fa23905b1e3d483b080dbea41a102
李吉元 <819491243@qq.com>
2016-11-01 21:57:51 +0800

    添加欢迎界面的跳转

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,12 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
@@ -10,5 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.iv_guide_register, R.id.iv_guide_login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_guide_register:
+                MFGT.gotoRegister(this);
+                break;
+            case R.id.iv_guide_login:
+                MFGT.gotoLogin(this);
+                break;
+        }
     }
 }
