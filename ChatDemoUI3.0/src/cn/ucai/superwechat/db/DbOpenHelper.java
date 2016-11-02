7594e625b4bd71f4680e299466a5e1e124603216
李吉元 <819491243@qq.com>
2016-11-02 17:05:59 +0800

    添加创建保存用户信息的数据的方法

--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/DbOpenHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/DbOpenHelper.java
@@ -53,7 +53,17 @@ public class DbOpenHelper extends SQLiteOpenHelper{
             + UserDao.PREF_TABLE_NAME + " ("
             + UserDao.COLUMN_NAME_DISABLED_GROUPS + " TEXT, "
             + UserDao.COLUMN_NAME_DISABLED_IDS + " TEXT);";
-	
+
+	private  static  final String CREATE_USER_TABLE_NAME = "CREATE TABLE "
+			+ UserDao.USER_TABLE_NAME + " ("
+			+ UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY, "
+			+ UserDao.USER_COLUMN_NICk + " TEXT, "
+			+ UserDao.USER_COLUMN_AVATAR_ID + " INTEGER, "
+			+ UserDao.USER_COLUMN_AVATAR_TYPE + " INTEGER, "
+			+ UserDao.USER_COLUMN_AVATA_PATH + " TEXT, "
+			+ UserDao.USER_COLUMN_AVATAR_SUFFIX + " TEXT, "
+			+ UserDao.USER_COLUMN_AVATAR_LASTAUPDATE_TIME + " TEXT);";
+
 	private DbOpenHelper(Context context) {
 		super(context, getUserDatabaseName(), null, DATABASE_VERSION);
 	}
@@ -75,6 +85,7 @@ public class DbOpenHelper extends SQLiteOpenHelper{
 		db.execSQL(INIVTE_MESSAGE_TABLE_CREATE);
 		db.execSQL(CREATE_PREF_TABLE);
 		db.execSQL(ROBOT_TABLE_CREATE);
+		db.execSQL(CREATE_USER_TABLE_NAME);
 		
 	}
 
